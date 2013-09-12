5570e06cb427ba020b9476231189c252f75ca6cd
carlos2505 <ccerdas@dgac.go.cr>
2013-09-12 12:18:58 -0600

    cambio 1

--- holaMundo/src/main/java/com/holaMundo/Conexion.java
+++ holaMundo/src/main/java/com/holaMundo/Conexion.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Conexion {
+	//coment
 
 	Connection myConnection;
 	String URL = "jdbc:postgresql://localhost:5432/inventario";
